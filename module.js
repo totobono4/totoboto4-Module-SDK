f57ad57ddfae6abb20ca1d0e05f3157a4603ba2b
totobono4 <totobono4@outlook.fr>
2023-04-17 01:15:36 +0200

    update lastest dependencies

--- module.js
+++ module.js
@@ -1,47 +1,46 @@
-const { Client, EmbedBuilder, MessageFlags, SlashCommandBuilder, SlashCommandSubcommandBuilder } = require('discord.js');
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js')
 
 class Module {
-  constructor() {
-    this.name = 'SDK-module';
-    this.version = '1.0.0';
+  constructor () {
+    this.name = 'SDK-module'
+    this.version = '1.0.0'
 
     this.commands = [
       new SlashCommandBuilder()
-      .setName('ping')
-      .setDescription('Replies with Pong!')
+        .setName('ping')
+        .setDescription('Replies with Pong!')
     ]
   }
 
   /**
-   * 
-   * @param {Client} client 
+   * @param {Client} client
    */
-  launch(client) {
-    client.on("interactionCreate", (interaction) => {
+  launch (client) {
+    client.on('interactionCreate', (interaction) => {
       if (!interaction.isChatInputCommand()) return
 
       switch (interaction.commandName) {
         case 'ping':
           this.ping(interaction)
-          break;
+          break
         default:
-          break;
+          break
       }
     })
   }
 
-  ping(interaction) {
-    const user = interaction.user;
-    const url = 'https://tenor.com/view/pong-video-game-atari-tennis-70s-gif-16894549';
-    const gif = 'https://c.tenor.com/2gyJVMt_L6wAAAAC/pong-video-game.gif';
+  ping (interaction) {
+    const user = interaction.user
+    const url = 'https://tenor.com/view/pong-video-game-atari-tennis-70s-gif-16894549'
+    const gif = 'https://c.tenor.com/2gyJVMt_L6wAAAAC/pong-video-game.gif'
     interaction.reply({
       embeds: [
         this.MessageEmbedBuilder(user, gif, 'ping', url, 'pong !', gif, 'totoboto4 ping services')
       ]
-    });
+    })
   }
 
-  MessageEmbedBuilder(author, thumbnail, title, url, description, image, footer) {
+  MessageEmbedBuilder (author, thumbnail, title, url, description, image, footer) {
     return new EmbedBuilder()
       .setColor('Navy')
       .setAuthor({
@@ -55,7 +54,7 @@ class Module {
       .setFooter({
         text: footer
       })
-      .setTimestamp(new Date());
+      .setTimestamp(new Date())
   }
 }
 
