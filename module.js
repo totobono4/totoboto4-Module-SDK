5955465aaff7c1ce6296c9b5cf9afdf0d53cf98e
totobono4 <totobono4@outlook.fr>
2023-09-10 21:36:40 +0200

    core centralisation

--- module.js
+++ module.js
@@ -1,61 +1,20 @@
-const { EmbedBuilder, SlashCommandBuilder } = require('discord.js')
+const { Client } = require('discord.js')
+const { Module } = require('totoboto4-core')
 
-class Module {
+class SDKModule extends Module {
   constructor () {
+    super()
+
     this.name = 'SDK-module'
     this.version = '1.0.0'
 
-    this.commands = [
-      new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Replies with Pong!')
-    ]
+    this.commands = []
   }
 
   /**
    * @param {Client} client
    */
-  launch (client) {
-    client.on('interactionCreate', (interaction) => {
-      if (!interaction.isChatInputCommand()) return
-
-      switch (interaction.commandName) {
-        case 'ping':
-          this.ping(interaction)
-          break
-        default:
-          break
-      }
-    })
-  }
-
-  ping (interaction) {
-    const user = interaction.user
-    const url = 'https://tenor.com/view/pong-video-game-atari-tennis-70s-gif-16894549'
-    const gif = 'https://c.tenor.com/2gyJVMt_L6wAAAAC/pong-video-game.gif'
-    interaction.reply({
-      embeds: [
-        this.MessageEmbedBuilder(user, gif, 'ping', url, 'pong !', gif, 'totoboto4 ping services')
-      ]
-    })
-  }
-
-  MessageEmbedBuilder (author, thumbnail, title, url, description, image, footer) {
-    return new EmbedBuilder()
-      .setColor('Navy')
-      .setAuthor({
-        name: author.username
-      })
-      .setThumbnail(thumbnail)
-      .setTitle(title)
-      .setURL(url)
-      .setDescription(description)
-      .setImage(image)
-      .setFooter({
-        text: footer
-      })
-      .setTimestamp(new Date())
-  }
+  launch (client) {}
 }
 
-module.exports = new Module()
+module.exports = new SDKModule()
