5b90f8dfb53f82e0cb9e37214100039ca44f4568
totobono4 <totobono4@outlook.fr>
2023-11-10 12:37:19 +0100

    Added dependencies to totoboto4-core

--- module.js
+++ module.js
@@ -1,5 +1,6 @@
-const { Client } = require('discord.js')
-const { Module } = require('totoboto4-core')
+const { Client, Events, SlashCommandBuilder } = require('discord.js')
+const { Module, Debugger } = require('totoboto4-core')
+const debug = new Debugger()
 
 class SDKModule extends Module {
   constructor () {
@@ -8,13 +9,26 @@ class SDKModule extends Module {
     this.name = 'SDK-module'
     this.version = '1.0.0'
 
-    this.commands = []
+    this.commands = [
+      new SlashCommandBuilder().setName("ping").setDescription("ping command")
+    ]
+
+    this.dependencies = [] // If dependencies, module names here
   }
 
   /**
-   * @param {Client} client
+   * @param {} data 
    */
-  launch (client) {}
+  launch(data) {
+    debug.addLayer("SDK", "sdk")
+    debug.debug(debug.layers.SDK, debug.types.Debug, "SDK Initialized !")
+
+    const client = data.client
+
+    client.on(Events.InteractionCreate, (interaction) => {
+      debug.debug(debug.layers.SDK, debug.types.Debug, "COMMAND !")
+    })
+  }
 }
 
 module.exports = new SDKModule()
